cf7702a481cf2b02ec7c76401638dcce72054a90
JJ Jacobs <jrjacobs24@gmail.com>
2021-09-30 12:47:46 -0400

    Basic types

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+const isOpen: boolean = false;
+
+const myName: string = 'JJ';
+
+const myAge: number = 31;
+
+const list: number[] = [0, 1, 2];
+
+// Tupals
+const me: [string, number, boolean] = ['Scott', 32, false];
+
+// Enum
+enum Job { WebDev, WebDesigner, PM }
+const job: Job = Job.WebDev;
+
+// Any
+const phone: any = 'Pixel';
+const tablet: any = 3;
